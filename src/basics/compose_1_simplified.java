ba76cbce84304a9a6e1fdcdeee1436b27bf6f0b4
ypokhrel <yubraj.pokhrel@insight.com>
2017-12-27 11:20:52 -0600

    simplify the function with lambda

--- src/basics/compose_1_simplified.java
+++ src/basics/compose_1_simplified.java
@@ -13,11 +13,6 @@ public class compose_1_simplified {
     }
 
     static Function<Integer, Integer> compose(Function<Integer, Integer> f1, Function<Integer, Integer> f2){
-        return new Function<Integer, Integer>() {
-            @Override
-            public Integer apply(Integer arg) {
-                return f1.apply(f2.apply(arg));
-            }
-        };
+        return arg -> f1.apply(f2.apply(arg));
     }
 }
